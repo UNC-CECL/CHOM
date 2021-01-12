834a7f3f99faafcaddc507a9b4f1c9f434c56700
katherine anarde laptop <UNCblahblahblah19!>
2021-01-12 12:10:24 -0500

    my first commit for chome

---
+++ chome.py
@@ -0,0 +1,7 @@
+import numpy as np
+import scipy.constants
+import yaml
+from numpy.lib.scimath import power as cpower, sqrt as csqrt
+from scipy.interpolate import interp1d
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import spsolve
