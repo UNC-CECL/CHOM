1068a80dd77117f518cf3f728f9c03a5fc2b4c6f
katherine anarde laptop <UNCblahblahblah19!>
2021-04-23 13:29:02 -0400

    started adding initial variables

--- chome.py
+++ chome.py
@@ -5,3 +5,55 @@ from numpy.lib.scimath import power as cpower, sqrt as csqrt
 from scipy.interpolate import interp1d
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
+
+
+class Chome:
+    def __init__(
+        self,
+        name="default",
+        total_time=400,
+        willingness_to_pay_min=1000,
+        willingness_to_pay_max=2000,
+    ):
+        """Coastal Home Ownership Model CHOM
+
+        Parameters
+        ----------
+        name: string, optional
+            Name of simulation
+        total_time: int, optional
+            Total time of simulation
+        willingness_to_pay_bounds: int, optional
+            Base willingness to pay distribution bounds
+
+
+        Examples
+        --------
+        >>> from chome import Chome
+        >>> datadir = "/Users/KatherineAnardeWheels/PycharmProjects/CASCADE/B3D_Inputs/"
+        >>> chome = Chome(datadir)
+        """
+
+        self._T = total_time
+
+        ###############################################################################
+        # owner-agent
+        ###############################################################################
+
+        self._mWTP = [willingness_to_pay_min, willingness_to_pay_max]
+
+        # agents/user-cost
+        self._rp_I = np.zeros(
+            1
+        )  # average risk premium real estate (same for investor and owner)
+
+        E = TaxParam(
+            "fieldNames",
+            "horizon",
+            "x0",
+            "amort",
+            "nourish_plan_horizon",
+            "expectation_horizon",
+            "share_oceanfront",
+            "share_owner",
+        )
