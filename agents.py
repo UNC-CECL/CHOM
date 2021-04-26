d19a21512bf2a38f23ffe533eaf673cbe9fdb422
katherine anarde laptop <UNCblahblahblah19!>
2021-04-26 18:45:59 -0400

    added agents.py class to initialize agent variables

---
+++ agents.py
@@ -0,0 +1,242 @@
+"""Agents
+
+This module does agent stuff (formerly agent_initialize.m and others)
+
+References
+----------
+
+.. [1] Jaap H. Nienhuis, Jorge Lorenzo Trueba; Simulating barrier island response to sea level rise with the barrier
+    island and inlet environment (BRIE) model v1.0 ; Geosci. Model Dev., 12, 4013–4030, 2019;
+    https://doi.org/10.5194/gmd-12-4013-2019
+
+
+Notes
+---------
+
+
+"""
+
+import numpy as np
+from scipy.stats import beta
+
+
+def agent_distribution(
+    rcov,
+    range_WTP_base,
+    range_WTP_alph,
+    range_tau_o,
+    range_rp_base,
+    beta_x,
+    n,
+):
+    # beta distribution max/min parameter values
+    beta_max = 10.5
+    beta_min = 2
+
+    bline2 = -1 * (beta_max - beta_min) * beta_x + beta_max
+    bline1 = (beta_max - beta_min) * beta_x + beta_min
+    # bline2   = -beta_x+(beta_max+beta_min);
+    # bline1   =  beta_x;
+
+    # covariances (KA: are these linear correlation parameters?)
+    r12 = rcov
+    r13 = rcov
+    r14 = rcov
+    r23 = 0.9
+    r24 = rcov
+    r34 = rcov
+
+    Rho = np.array(
+        [[1, r12, r13, r14], [r12, 1, r23, r24], [r13, r23, 1, r34], [r14, r24, r34, 1]]
+    )
+    # U = copularnd('Gaussian', Rho, n);
+    #  gaussian copula is based on the Gaussian Multivariate distribution
+    size = Rho.shape[0]
+    means = np.zeros(size)
+    U = np.random.multivariate_normal(means, Rho, size=n)
+
+    # X = [betainv(U(:, 1), bline1, bline2) betainv(U(:, 2), bline1, bline2) betainv(U(:, 3), bline1, bline2) betainv(
+    #     U(:, 4), bline1, bline2)];
+    X = [
+        beta.ppf(U[:, 0], bline1, bline2),
+        beta.ppf(U[:, 1], bline1, bline2),
+        beta.ppf(U[:, 2], bline1, bline2),
+        beta.ppf(U[:, 3], bline1, bline2),
+    ]
+
+    tau_o = X[0]
+    WTP_base = X[1]
+    WTP_alph = X[2]
+    rp_base = X[3]
+
+    # rescale to property intervals
+    tau_o = tau_o * (range_tau_o[1] - range_tau_o[0])
+    tau_o = tau_o + range_tau_o[0]
+
+    WTP_base = WTP_base * (range_WTP_base[1] - range_WTP_base[0])
+    WTP_base = WTP_base + range_WTP_base[0]
+
+    WTP_alph = WTP_alph * (range_WTP_alph[1] - range_WTP_alph[0])
+    WTP_alph = WTP_alph + range_WTP_alph[0]
+
+    rp_base = rp_base * (range_rp_base[1] - range_rp_base[0])
+    rp_base = rp_base + range_rp_base[0]
+
+    return tau_o, WTP_base, rp_base, WTP_alph
+
+
+def agent_distribution_adjust():
+    return
+
+
+class Agents:
+    def __init__(
+        self,
+        T=400,
+        n=2500,
+        bta=0.2,
+        m=0.003,  # which of these will be tuned for locations? keep here, otherwise, move below
+        delta=0.06,
+        gam=0.01,
+        HV=1000,
+        rp_storm=0,
+        epsilon=1,
+        tau_c=0.2,
+        capgain_feedbackparam=1e-10,
+        beta_x_feedbackparam=1e-10,
+        adjust_beta_x=5e-8,
+        risk_to_EnvExptdGains=0.2,
+        frac_realist=0.75,
+        rcov=0.8,
+        env_risk_immediacy=0.2,
+    ):
+
+        """These variables were formerly contained in structures "A" and "X"
+
+        Examples
+        --------
+        >>> from agents import Agents
+        >>> agents_doing_thangs = Agents()
+
+        Parameters
+        ----------
+        bta: float, optional
+            DESCRIPTION GOES HERE
+        m: float, optional
+            Additional investor-only fees of renting the propoerty (just investors)
+        delta: float, optional
+            Interest rate (same for investor and owner)
+        gam: float, optional
+            Depreciation rate on housing capital (same for investor and owner)
+        HV: int, optional
+            Annualized value of housing services (same for investor and owner)
+        rp_storm: int, optional
+            NEED DESCRIPTION
+        epsilon: int, optional
+            Additional bid for investor
+        tau_c: float, optional
+            Corporate tax rate (just investors) -- U.S. federal rate (could add 2.5# for NC)
+        capgain_feedbackparam: float, optional
+            NEED DESCRIPTION
+        beta_x_feedbackparam: int, optional
+            NEED DESCRIPTION
+        adjust_beta_x: int, optional
+            NEED DESCRIPTION
+        risk_to_EnvExptdGains: int, optional
+            NEED DESCRIPTION
+        frac_realist: int, optional
+            NEED DESCRIPTION
+        rcov: int, optional
+            NEED DESCRIPTION
+        env_risk_immediacy: int, optional
+            Determines how quickly people switch from abitrage to environmental risk in capital gains
+        """
+
+        self._T = T
+        self._n = n
+        self._bta = bta
+        self._m = m
+        self._delta = delta
+        self._gam = gam
+        self._HV = HV
+        self._rp_storm = rp_storm
+        self._epsilon = epsilon
+        self._tau_c = tau_c
+        self._capgain_feedbackparam = capgain_feedbackparam
+        self._beta_x_feedbackparam = beta_x_feedbackparam
+        self._adjust_beta_x = adjust_beta_x
+        self._risk_to_EnvExptdGains = risk_to_EnvExptdGains
+        self._frac_realist = frac_realist
+        self._rcov = rcov
+        self._env_risk_immediacy = env_risk_immediacy
+
+        RNG = np.random.default_rng(
+            seed=self._n
+        )  # KA: added seeded random number generator the size of n
+
+        # owner agent
+        if self._bta == 2:
+            # base willingness to pay distribution bounds
+            self._range_WTP_base = [5000, 35000]
+            self._range_WTP_alph = [5000, 35000]
+            self._range_tau_o = [0.0, 0.35]  # income tax bracket (0 to 0.37)
+        else:
+            self._range_WTP_base = [5000, 25000]
+            self._range_WTP_alph = [5000, 25000]
+            self._range_tau_o = [0.0, 0.35]
+
+        if self._bta == 0.2:
+            # base risk premium distribution - random distribution of shifts to base risk premium
+            self._range_rp_base = [-0.03, 0.03]
+        else:
+            self._range_rp_base = [0, 0.03]
+
+        # average risk premium real estate (same for investor and owner)
+        self._rp_I = np.zeros(1)
+        self._rp_o = np.zeros(self._n)
+
+        # base property tax rate (same for investor and owner)
+        self._tau_prop = 0.01 * np.ones(self._T)
+
+        if bta == 2:
+            self._beta_x = 0.4
+        else:
+            self._beta_x = 0.4
+
+        [
+            self._tau_o,
+            self._WTP_base,
+            self._rp_base,
+            self._WTP_alph,
+        ] = agent_distribution(
+            self._rcov,
+            self._range_WTP_base,
+            self._range_WTP_alph,
+            self._range_tau_o,
+            self._range_rp_base,
+            self._beta_x,
+            n,
+        )  # generate agent variables
+
+        self._rp_base_sorted = np.sort(self._rp_base)
+        self._I = np.argsort(self._rp_base)
+
+        self._I_realist = np.zeros(len(self._rp_base))
+        self._I_realist[
+            self._I[-1 - round(len(self._rp_base) * self._frac_realist) + 1 : -1]
+        ] = 1  # KA -- make sure this matches matlab re: indices
+
+        self._g_I = 0.01
+        self._g_o = 0.04 * RNG.standard_normal(self._n)  # randn(n,1)
+        self._price = np.zeros(self._T)
+        self._rent = np.zeros(self._T)
+        self._mkt = np.zeros(self._T)
+        self._t_halftime = np.zeros(self._T)
+
+        if self._bta >= 0.2:
+            self._price[0] = 6e5
+            self._rent[0] = 1e5
+        else:
+            self._price[0] = 4e5
+            self._rent[0] = 1e5
+        self._mkt[0] = 0.4
--- chome.py
+++ chome.py
@@ -5,55 +5,177 @@ from numpy.lib.scimath import power as cpower, sqrt as csqrt
 from scipy.interpolate import interp1d
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
+from agents import Agents
 
-test
+
 class Chome:
     def __init__(
         self,
         name="default",
+        total_number_of_agents=2500,
+        agent_expectations_time_horizon=30,
+        agent_erosion_update_weight=0.5,
+        barrier_island_height=1,
+        beach_width_beta_oceanfront=0.2,
+        beach_width_beta_nonoceanfront=0.1,
+        beach_nourishment_fill_depth=10,
+        beach_nourishment_fill_width=2000,
+        beach_full_cross_shore=100,
+        discount_rate=0.06,
+        dune_height_build=4,
+        external_housing_market_value_oceanfront=6e5,
+        external_housing_market_value_nonoceanfront=4e5,
+        fixed_cost_beach_nourishment=4e6,
+        fixed_cost_dune_nourishment=1e6,
+        nourishment_cost_subsidy=12e6,
+        nourishment_plan_loan_amortization_length=5,
+        nourishment_plan_time_commitment=10,
+        share_oceanfront=0.25,
+        shoreline_retreat_rate=4,
+        storm_frequency=1 / 20,
+        sand_cost=10,
         total_time=400,
+        taxratio_oceanfront=3,
         willingness_to_pay_min=1000,
         willingness_to_pay_max=2000,
     ):
         """Coastal Home Ownership Model CHOM
-
         Parameters
         ----------
         name: string, optional
             Name of simulation
+        total_number_of_agents: int, optional
+            Total number of agents in simulation
         total_time: int, optional
             Total time of simulation
-        willingness_to_pay_bounds: int, optional
-            Base willingness to pay distribution bounds
-
-
+        beach_width_beta_oceanfront: float, optional
+            Hedonic beach width coefficient for oceanfront housing
+        beach_width_beta_nonoceanfront: float, optional
+            Hedonic beach width coefficient for non-oceanfront housing
+        share_oceanfront: float, optional
+            Fraction of agents/housing units in oceanfront (front row)
+        taxratio_oceanfront: float, optional
+            The proportion of tax burden placed on oceanfront row for beach management
+        agent_erosion_update_weight: float, optional
+            Agent's perceived erosion rate update. 0=never update, 1=instantaneous erosion rate
+        storm_frequency: float, optional
+            Average frequency of major storms
+        sand_cost: int, optional
+            Unit cost of sand $/m^3
+        fixed_cost_beach_nourishment: int, optional
+            Fixed cost of 1 nourishment project
+        fixed_cost_dune_nourishment: int, optional
+            Fixed cost of building dunes once
+        nourishment_cost_subsidy: int, optional
+            Subsidy on cost of entire nourishment plan
+        external_housing_market_value_oceanfront:  , optional
+            Value of comparable housing option outside the coastal system
+        external_housing_market_value_nonoceanfront:  , optional
+            Value of comparable housing option outside the coastal system
+        agent_expectations_time_horizon: int, optional
+            Time horizon over which agent's consider physical environment
+        nourishment_plan_loan_amortization_length: int, optional
+            Number of years over which homeowners pay back nourishment cost
+        nourishment_plan_time_commitment: int, optional
+            Time span of nourishment plan (i.e. multiple nourishments over multiple X years)
+        beach_nourishment_fill_depth: int, optional
+            Average depth (meters) of nourishment volume place on shoreface
+        beach_nourishment_fill_width: int, optional
+            Average alongshore width (meters) of nourishment volume place on shoreface
+        beach_full_cross_shore: int, optional
+            The cross-shore extent (meters) of fully nourished beach
+        discount_rate: float, optional
+            Rate at which future flows of value are discounted
+        dune_height_build: int, optional
+            Height (meters) of fully built dunes
+        barrier_island_height: int, optional
+            Height of barrier island (meters) with respect to fixed reference point
         Examples
         --------
         >>> from chome import Chome
-        >>> datadir = "/Users/KatherineAnardeWheels/PycharmProjects/CASCADE/B3D_Inputs/"
-        >>> chome = Chome(datadir)
+        >>> chome = Chome()
         """
 
+        self._name = name
+        self._n = total_number_of_agents
         self._T = total_time
+        self._bta_OF = beach_width_beta_oceanfront
+        self._bta_NOF = beach_width_beta_nonoceanfront
+        self._share_OF = share_oceanfront
+        self._taxratio_OF = taxratio_oceanfront
+        self._theta_er = agent_erosion_update_weight
+        self._lam_storm = storm_frequency
+        self._sandcost = sand_cost
+        self._fixedcost_beach = fixed_cost_beach_nourishment
+        self._fixedcost_dune = fixed_cost_dune_nourishment
+        self._nourish_subsidy = nourishment_cost_subsidy
+        self._P_e_OF = external_housing_market_value_oceanfront
+        self._P_e_NOF = external_housing_market_value_nonoceanfront
+        self._expectation_horizon = agent_expectations_time_horizon
+        self._amort = nourishment_plan_loan_amortization_length
+        self._nourish_plan_horizon = nourishment_plan_time_commitment
+        self._Ddepth = beach_nourishment_fill_depth
+        self._lLength = beach_nourishment_fill_width
+        self._x0 = beach_full_cross_shore
+        self._delta_disc = discount_rate
+        self._h0 = dune_height_build
+        self._barr_elev = barrier_island_height
 
         ###############################################################################
-        # owner-agent
+        # dependent variables: owner-agent
         ###############################################################################
 
         self._mWTP = [willingness_to_pay_min, willingness_to_pay_max]
+        self._P_e_OF = external_housing_market_value_oceanfront + np.zeros(self._T)
+        self._P_e_NOF = external_housing_market_value_nonoceanfront + np.zeros(self._T)
+        self._n_NOF = round(self._n * (1 - self._share_OF))
+        self._n_OF = round(self._n * self._share_OF)
+        self._n_agent_total = self._n
+        self._Ebw = np.zeros(self._T)
+        self._Edh = np.zeros(self._T)
+        self._I_OF = np.zeros(self._n_agent_total)
+        self._I_OF[self._n_NOF + 1 : -1] = 1  # KA: check that this is right in Matlab
+        self._I_own = np.zeros(self._n_agent_total)
 
-        # agents/user-cost
-        self._rp_I = np.zeros(
-            1
-        )  # average risk premium real estate (same for investor and owner)
+        ###############################################################################
+        # physical parameters
+        ###############################################################################
 
-        E = TaxParam(
-            "fieldNames",
-            "horizon",
-            "x0",
-            "amort",
-            "nourish_plan_horizon",
-            "expectation_horizon",
-            "share_oceanfront",
-            "share_owner",
+        self._beach_plan = 11 + np.zeros(self._T)
+        self._h_dune = np.zeros(self._T)
+        self._h_dune[0] = self._h0
+        self._bw = np.zeros(self._T)
+        self._bw[0] = self._x0
+        self._E_ER = np.zeros(self._T)
+        self._ER = shoreline_retreat_rate + np.zeros(self._T)
+        self._nourishtime = np.zeros(self._T)
+        self._newplan = np.zeros(self._T)
+        self._builddunetime = np.zeros(self._T)
+        self._msl = np.zeros(self._T)
+
+        # storms
+        self._RNG = np.random.default_rng(
+            seed=self._n
+        )  # KA: added seeded random number generator the size of n
+        self._storms = self._RNG.poisson(
+            lam=self._lam_storm, size=self._T
+        )  # poissrnd(lam_storm,T,1)
+
+        ###############################################################################
+        # agents/user cost
+        ###############################################################################
+
+        # average risk premium real estate (same for investor and owner)
+        self._rp_I = np.zeros(1)
+        self._agents_front_row = Agents(
+            self._T,
+            self._n_OF,
+            self._bta_OF,
         )
+        self._agents_back_row = Agents(
+            self._T,
+            self._n_NOF,
+            self._bta_NOF,
+        )
+
+        self._nourishment_off = 0
